7c0a37c32fe754cf687ea7e86f3a4cece198216d
Spokiy <maksimchesnokovstudent@gmail.com>
2023-07-24 13:30:59 +0300

    Category

---
+++ category.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Collections; 
+
+namespace Category
+{
+    class Category
+    {
+        public int Id{ get; set; }
+        public string Name { get; set; }
+        public double price { get; set; } 
+    }
+}
