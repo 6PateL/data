565b98387d9b44d2a3b5ea831479a30570b91070
Spokiy <maksimchesnokovstudent@gmail.com>
2023-07-24 14:01:37 +0300

    New function

--- category.cs
+++ category.cs
@@ -9,5 +9,9 @@ namespace Category
         public int Id{ get; set; }
         public string Name { get; set; }
         public double price { get; set; } 
+        public void SayHello()
+        {
+            Console.WriteLine("Hello World");
+        }
     }
 }
